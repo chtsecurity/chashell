58f3be5850063532d17701d367c5cc07d884d3fd
zodius <zodius.chen@gmail.com>
2023-06-16 11:29:49 +0800

    feat: :bug: Using custom resolver
    chashell client使用custom resolver避免目標主機沒設定DNS，目前使用hardcoded 8.8.8.8

--- lib/transport/dnsclient.go
+++ lib/transport/dnsclient.go
@@ -1,12 +1,25 @@
 package transport
 
 import (
+	"context"
 	"fmt"
 	"net"
+	"time"
 )
 
 func sendDNSQuery(data []byte, target string) (responses []string, err error) {
+	// Custom resolver
+	resolver := net.Resolver{
+		PreferGo: true,
+		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+			d := net.Dialer{
+				Timeout: time.Millisecond * time.Duration(10000),
+			}
+			return d.DialContext(ctx, network, "8.8.8.8:53")
+		},
+	}
+
 	// We use TXT requests to tunnel data. Feel free to implement your own method.
-	responses, err = net.LookupTXT(fmt.Sprintf("%s.%s", data, target))
+	responses, err = resolver.LookupTXT(context.Background(), fmt.Sprintf("%s.%s", data, target))
 	return
 }
